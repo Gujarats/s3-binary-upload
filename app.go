0a11445cde812cde25c228f628759686b7afa93c
Gujarat Santana <gujarat.santana@traveloka.com>
2018-02-03 09:40:22 +0700

    main logic to get artifact

--- app.go
+++ app.go
@@ -2,56 +2,74 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// TODO fill these in!
 const (
 	S3_REGION = "ap-southeast-1"
 	S3_BUCKET = "s3-website-test.hashicorp.com"
 )
 
+var gradleCacheDir = "~/.gradle/caches/modules-2/files-2.1"
+
 func main() {
-	// get all the files from the specific folder
-	//files, err := ioutil.ReadDir(".")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//for _, file := range files {
-	//	fmt.Println(file.Name())
-	//}
-
-	//searchDir := "."
-
-	//fileList := []string{}
-	//filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-	//	fileList = append(fileList, path)
-	//	return nil
-	//})
-
-	//for _, file := range fileList {
-	//	fmt.Println(file)
-	//}
-
 	// Create a single AWS session (we can re use this if we're uploading many files)
 	s, err := session.NewSession(&aws.Config{Region: aws.String(S3_REGION)})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Upload
-	//err = AddFileToS3(s, "test/com.haha.bro/testing/result.csv")
-	err = AddFileToS3(s, "com.helloworld.common/accessor/1.0.0/file.pom")
+	// get package name
+	fmt.Print("\nEnter your package = ")
+	var packageName string
+	_, err = fmt.Scan(&packageName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// list all the directory names
+	packageNames, err := exec.Command("ls").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// get specific directory for scanning artifact
+	packages := filterDir(packageNames, packageName)
+	for _, pack := range packages {
+		files := getFilesFrom(pack)
+		for _, file := range files {
+			// Upload
+			err = AddFileToS3(s, file)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+}
+
+func getFilesFrom(rootDir string) []string {
+	var files []string
+	filepath.Walk(gradleCacheDir+rootDir,
+		func(path string, f os.FileInfo, err error) error {
+			if !f.IsDir() {
+				files = append(files, path)
+			}
+			return nil
+		},
+	)
+
+	return files
 }
 
 // AddFileToS3 will upload a single file to S3, it will require a pre-built aws session
@@ -71,11 +89,14 @@ func AddFileToS3(s *session.Session, fileDir string) error {
 	buffer := make([]byte, size)
 	file.Read(buffer)
 
+	// TODO modify the fileDirectory to custome dir so it can be downloaded using gradle
+	newFileDir := removeEncryptPath(fileDir)
+
 	// Config settings: this is where you choose the bucket, filename, content-type etc.
 	// of the file you're uploading.
 	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
 		Bucket:               aws.String(S3_BUCKET),
-		Key:                  aws.String(fileDir),
+		Key:                  aws.String(newFileDir),
 		ACL:                  aws.String("private"),
 		Body:                 bytes.NewReader(buffer),
 		ContentLength:        aws.Int64(size),
