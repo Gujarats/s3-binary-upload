f24d4224894e9f7ae037657882da8832a26967b1
Gujarat Santana <gujarat.santana@traveloka.com>
2018-02-05 10:35:13 +0700

    create folder builder for given source directory

---
+++ folder-builder.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func folderBuilder(source string) string {
+	var result string
+	folders := removeDotToSlash(source)
+	for index, folder := range folders {
+		if index != len(folders)-1 {
+			result += folder + "/"
+		} else {
+			result += folder
+		}
+
+	}
+
+	return result
+}
+
+// get root folder and subfolders from given path of artifact
+func removeDotToSlash(source string) []string {
+	var result []string
+	re := regexp.MustCompile("^\\d+(\\.\\d+)*$")
+	splitSlashes := strings.Split(source, slash)
+	for index, splitSlash := range splitSlashes {
+		// avoid splitting the dots for the filename
+		if index == len(splitSlashes)-1 && len(splitSlashes) != 1 {
+			result = append(result, splitSlash)
+			continue
+		}
+
+		// avoid spliting removing the dot fro the number
+		if re.MatchString(splitSlash) {
+			result = append(result, splitSlash)
+			continue
+		}
+
+		splitDots := strings.Split(splitSlash, dot)
+		for _, splitDot := range splitDots {
+			result = append(result, splitDot)
+		}
+	}
+	return result
+}
+
+func removeEncryptPath(pathFile string) string {
+	var result string
+	splitPathFiles := strings.Split(pathFile, slash)
+	for index, splitPathFile := range splitPathFiles {
+		if index != (len(splitPathFiles) - 2) {
+			if index == 0 {
+				result = splitPathFile
+			} else {
+				result = result + "/" + splitPathFile
+			}
+		}
+	}
+	return result
+}
---
+++ folder-builder_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFolderBuilder(t *testing.T) {
+	testObjects := []struct {
+		source   string
+		expected string
+	}{
+		{
+			source:   "com.helloworld.common.gradle/3.01/java.jar",
+			expected: "com/helloworld/common/gradle/3.01/java.jar",
+		},
+		{
+			source:   "com.helloworld.common.gradle/3.01/java.jar",
+			expected: "com/helloworld/common/gradle/3.01/java.jar",
+		},
+	}
+
+	for _, testObject := range testObjects {
+		actual := folderBuilder(testObject.source)
+		if actual != testObject.expected {
+			t.Errorf("expected = %+v, actual = %+v\n", testObject.expected, actual)
+		}
+	}
+}
+
+func TestDotChanger(t *testing.T) {
+	testObjects := []struct {
+		source   string
+		expected []string
+	}{
+		{
+			source:   "com.helloworld.common.gradle",
+			expected: []string{"com", "helloworld", "common", "gradle"},
+		},
+
+		{
+			source:   "com.helloworld.common.gradle/library",
+			expected: []string{"com", "helloworld", "common", "gradle", "library"},
+		},
+
+		{
+			source:   "com.hello-world.common.gradle/library/java-plugin/4.10.0",
+			expected: []string{"com", "hello-world", "common", "gradle", "library", "java-plugin", "4.10.0"},
+		},
+
+		{
+			source:   "com.hello-world.p2p.gradle/library/java-plugin/4.10.0",
+			expected: []string{"com", "hello-world", "p2p", "gradle", "library", "java-plugin", "4.10.0"},
+		},
+	}
+
+	for _, testObject := range testObjects {
+		actual := removeDotToSlash(testObject.source)
+		if !reflect.DeepEqual(actual, testObject.expected) {
+			t.Errorf("expected = %+v, actual = %+v\n", testObject.expected, actual)
+		}
+	}
+}
+
+func TestRemoveEncPath(t *testing.T) {
+	testObjects := []struct {
+		source   string
+		expected string
+	}{
+		{
+			source:   "com.helloworld.common/accessor/1.0.0/16aafadb75b1ad0eacf6e3fef68320e6502df136/file.pom",
+			expected: "com.helloworld.common/accessor/1.0.0/file.pom",
+		},
+
+		{
+			source:   "com.helloworld.common/accessor/1.0.0/16aafadb75b1ad0eacf6e3fef68320e6502df136/file.pom",
+			expected: "com.helloworld.common/accessor/1.0.0/file.pom",
+		},
+	}
+
+	for _, testObject := range testObjects {
+		actual := removeEncryptPath(testObject.source)
+		if actual != testObject.expected {
+			t.Errorf("expected = %+v, actual = %+v\n", testObject.expected, actual)
+		}
+	}
+}
