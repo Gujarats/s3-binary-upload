80450814008a881078a8291b0b01e516ec01a0ad
Gujarats <gujarat.santana@gmail.com>
2018-03-23 14:15:05 +0700

    fix config

--- config.go
+++ config.go
@@ -6,18 +6,23 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	gradleCacheDir = "/.gradle/caches/modules-2/files-2.1"
+)
+
 type Config struct {
 	S3Buckets []string `viper:"s3Buckets"`
 	S3Bucket  string   `viper:"s3Bucket"`
 	Region    string   `viper:"region"`
 	Profile   string   `viper:"profile"`
 
-	GradleCacheDir string `viper:"gradle"`
-	ArtfactsDir    string `viper:"artfactsDir"`
+	GradleCacheDir      []string `viper:"gradle"`
+	ArtfactsDirectories []string `viper:"artfactsDirectories"`
 
-	UploadArtifacs   bool     `viper:"uploadArtifacs"`
-	DownloadArtifacs bool     `viper:"downloadArtifacs"`
-	LinkArtifacts    []string `viper:"linkArtifacts"`
+	UploadArtifacs    bool     `viper:"uploadArtifacs"`
+	DownloadArtifacs  bool     `viper:"downloadArtifacs"`
+	ArtifactsLocation string   `viper:"artifactsLocation"`
+	LinkArtifacts     []string `viper:"linkArtifacts"`
 
 	// authentication for the provided link artifacts
 	Username string `viper:"username"`
@@ -28,16 +33,13 @@ func getConfig() *Config {
 	viper.SetConfigName("config")                  // name of config file (without extension)
 	viper.AddConfigPath("$HOME/.s3-binary-upload") // call multiple times to add many search paths
 	viper.AddConfigPath(".")                       // optionally look for config in the working directory
-	viper.SetDefault("gradle", "/.gradle/caches/modules-2/files-2.1")
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
+	err := viper.ReadInConfig()                    // Find and read the config file
+	if err != nil {                                // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file must exist in ~/.s3-binary-upload/config.yaml: %s \n", err))
 	}
 
 	// read the config file to struct
-	config := &Config{
-		GradleCacheDir: viper.GetString("gradle"),
-	}
+	config := &Config{}
 	err = viper.Unmarshal(config)
 	if err != nil {
 		panic(fmt.Errorf("Fatal error unmarhsal config struct : %s \n", err))
